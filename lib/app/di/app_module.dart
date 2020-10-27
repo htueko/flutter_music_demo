fbaa468c8116180f98fea01949dc11cd4525a45d
Htue Ko <htooko1101@gmail.com>
2020-10-27 23:01:06 +0630

    setup dependency injection

--- lib/app/di/app_module.dart
+++ lib/app/di/app_module.dart
@@ -1,1 +1,13 @@
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
 
+import 'app_module.config.dart';
+
+/// flutter pub run build_runner watch --delete-conflicting-outputs
+/// instance to GitIt
+GetIt locator = GetIt.instance;
+
+@injectableInit
+void setupLocator() {
+  $initGetIt(locator);
+}
